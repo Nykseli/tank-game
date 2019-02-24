70f7c5f7c2cb62660f268d619e18669c80853b78
Miika Alikirri <miika.alik@gmail.com>
2019-02-24 16:44:05 +0200

    Tank can now move somewhat properly in the user created map

--- html/sketch.js
+++ html/sketch.js
@@ -1,20 +1,26 @@
 const GAME_WIDTH = 1280;
 const GAME_HEIGHT = 720;
 
+let gameMap;
 let tank1;
+
 
 function setup() {
     createCanvas(GAME_WIDTH, GAME_HEIGHT);
     frameRate(20);
 
+    gameMap = new GameMap();
+
     tank1 = new Tank();
-    tank1.setoffset(1100, GAME_HEIGHT / 2 - tank1.height);
+    tank1.setoffset(800, GAME_HEIGHT / 2 - tank1.height);
+    // gameMap.draw();
+    // tank1.draw();
 }
 
 function draw() {
     clear();
-    drawGameLevel();
-
+    //drawGameLevel();
+    gameMap.draw();
 
     if (keyIsDown(LEFT_ARROW)) {
         tank1.moveLeft();
